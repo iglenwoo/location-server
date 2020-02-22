13df24d11af37fd81b2b4238ab48d3c8b5dccca1
Ingyu Woo <iglenwoo@gmail.com>
2020-02-21 21:28:44 -0800

    connect redis with default option

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const router = require('../routes')
+const assert = require('assert')
+const redis = require('redis')
+const { AbortError, AggregateError, ReplyError } = require("redis");
+const client = redis.createClient()
+client.on("error", function(err) {
+  console.log('Redis Error ' + err)
+
+  assert(err instanceof Error);
+  assert(err instanceof AbortError);
+  assert(err instanceof AggregateError);
+
+  // The set and get are aggregated in here
+  assert.strictEqual(err.errors.length, 2);
+  assert.strictEqual(err.code, "NR_CLOSED");
+});
 
 const app = express()
 
